a0b8a92d490f373a6d05f0a2d16fc41a700c44a3
Will Maethner <wmaethner@developertown.com>
2021-04-19 10:48:58 -0400

    Update UserService.cs

--- PickEmLeagueServices/Services/UserService.cs
+++ PickEmLeagueServices/Services/UserService.cs
@@ -28,7 +28,7 @@ namespace PickEmLeagueServices.Services
 
         public IEnumerable<User> GetUsers()
         {
-            return new List<User>(); //_dbContext.Users.ToList();
+            return _dbContext.Users.ToList();
         }
 
         public User GetUser(Guid guid)
