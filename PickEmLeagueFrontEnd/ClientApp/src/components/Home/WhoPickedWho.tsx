f56840d2ab524de090ad4fab368dd64cb8953b28
Will Maethner <wmaethner@gmail.com>
2021-10-11 21:26:33 -0400

    Add average wager amount to Who Picked Who
    Rearrange who picked who page as well. Two columns, games on top of each other in a box (ESPN style)

--- PickEmLeagueFrontEnd/ClientApp/src/components/Home/WhoPickedWho.tsx
+++ PickEmLeagueFrontEnd/ClientApp/src/components/Home/WhoPickedWho.tsx
@@ -7,12 +7,13 @@ import { useGetGamesByWeek } from "../../Data/Game/useGetGamesByWeek";
 import { useGetGamePicksByWeek } from "../../Data/GamePicks/useGetGamePicksByWeek";
 import { TeamDisplay } from "../Teams/TeamDisplay";
 import { WeekSelector } from "../Week/WeekSelector";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
-type CollapsibleTeamAndPicksProps = {
-  users: (User | undefined)[];
+type CollapsibleGamePicksDisplayProps = {
   game: Game;
-  result: GameResult;
-};
+  gamePicks: GamePick[];
+}
 
 export function WhoPickedWho() {
   const { week } = useContext(WeekContext);
@@ -27,20 +28,17 @@ export function WhoPickedWho() {
     GetGamePicks();
   }, [week]);
 
-  function getFinishedGames() {
-    return games.filter((g) => g.gameResult !== GameResult.NotPlayed);
-  }
-
-  function getInProgressGames() {
+  function getStartedGames() {
     return games.filter(
-      (g) => g.gameResult === GameResult.NotPlayed && g.hasStarted
+      (g) => g.hasStarted
     );
   }
 
-  function getWhoPickedGameResult(game: Game, result: GameResult) {
-    return gamePicks
-      .filter((gp) => gp.game?.id == game.id && gp.pick === result)
-      .map((gp) => gp.user);
+  function getHalfOfStartedGames(first: boolean) {
+    const list = getStartedGames();
+    const half = Math.ceil(list.length / 2);
+
+    return first ? list.slice(0, half) : list.slice(-half);
   }
 
   return (
@@ -50,50 +48,22 @@ export function WhoPickedWho() {
       <Container>
         <Row>
           <Col className="col-6 text-center">
-            <h2>Finished Games</h2>
-            {getFinishedGames().map((g) => (
+            {getHalfOfStartedGames(true).map((g) => (
               <Row key={g.id}>
-                <Col className="col-5">
-                  <CollapsibleTeamAndPicks
-                    users={getWhoPickedGameResult(g, GameResult.AwayWin)}
-                    game={g}
-                    result={GameResult.AwayWin}
-                  />
-                </Col>
-                <Col className="col-2">
-                  <h4>@</h4>
-                </Col>
-                <Col className="col-5">
-                  <CollapsibleTeamAndPicks
-                    users={getWhoPickedGameResult(g, GameResult.HomeWin)}
-                    game={g}
-                    result={GameResult.HomeWin}
-                  />
-                </Col>
+                <CollapsibleGamePicksDisplay
+                  game={g}
+                  gamePicks={gamePicks.filter(gp => gp.game?.id == g.id)}
+                />
               </Row>
             ))}
           </Col>
           <Col className="col-6 text-center">
-            <h2>In Progress Games</h2>
-            {getInProgressGames().map((g) => (
+            {getHalfOfStartedGames(false).map((g) => (
               <Row key={g.id}>
-                <Col className="col-5">
-                  <CollapsibleTeamAndPicks
-                    users={getWhoPickedGameResult(g, GameResult.AwayWin)}
-                    game={g}
-                    result={GameResult.AwayWin}
-                  />
-                </Col>
-                <Col className="col-2">
-                  <h4>@</h4>
-                </Col>
-                <Col className="col-5">
-                  <CollapsibleTeamAndPicks
-                    users={getWhoPickedGameResult(g, GameResult.HomeWin)}
-                    game={g}
-                    result={GameResult.HomeWin}
-                  />
-                </Col>
+                <CollapsibleGamePicksDisplay
+                  game={g}
+                  gamePicks={gamePicks.filter(gp => gp.game?.id == g.id)}
+                />
               </Row>
             ))}
           </Col>
@@ -103,35 +73,69 @@ export function WhoPickedWho() {
   );
 }
 
-function CollapsibleTeamAndPicks(props: CollapsibleTeamAndPicksProps) {
-  const [open, setOpen] = useState(false);
+function CollapsibleGamePicksDisplay(props: CollapsibleGamePicksDisplayProps) {
+  const [homeOpen, setHomeOpen] = useState(false);
+  const [awayOpen, setAwayOpen] = useState(false);
+
+  function getWhoPickedGameResult(result: GameResult) {
+    return props.gamePicks
+      .filter((gp) => gp.pick === result)
+      .map((gp) => gp.user);
+  }
+
+  function averageScore(game: Game, result: GameResult) {
+    let picks = props.gamePicks.filter((gp) => gp.game?.id == game.id && gp.pick === result);
+
+    if (picks.length == 0) { return 0; }
+
+    const total = picks.map(gp => gp.wager)
+      .reduce(function (a, b) {
+        return a! + b!;
+      });
+
+    return total! / picks.length;
+  }
 
   function getClassName(result: GameResult) {
     var align = result === GameResult.AwayWin ? "text-left" : "text-right";
-    return ["col-8", align].join(" ");
+    return ["col-6", align].join(" ");
   }
 
-  function getStyle(result: GameResult) {
-    return result === props.game.gameResult ? "limegreen" : "";
+  function getStyle(game: Game, result: GameResult) {
+    return result === game.gameResult ? "limegreen" : "";
   }
 
-  return props.result === GameResult.AwayWin ? (
-    <Container>
-      <Row>
+  return (
+    <Container className="align-items-center border">
+      <Row className="align-items-center">
         <Col
           className={getClassName(GameResult.AwayWin)}
-          style={{ backgroundColor: getStyle(GameResult.AwayWin) }}
+          style={{ backgroundColor: getStyle(props.game, GameResult.AwayWin) }}
         >
           <TeamDisplay id={props.game.awayTeamId!} />
         </Col>
-        <Col className="col-4">
-          <Button onClick={() => setOpen(!open)}>{props.users.length}</Button>
+        <Col className="col-3">
+          <h4>{averageScore(props.game, GameResult.AwayWin)} pts</h4>
+        </Col>
+        <Col className="col-3">
+          <Row>
+            <Col className="col-6">
+              <h4>
+                {getWhoPickedGameResult(GameResult.AwayWin).length}
+              </h4>
+            </Col>
+            <Col className="col-6">
+              <Button onClick={() => setAwayOpen(!awayOpen)}>
+                <FontAwesomeIcon icon={awayOpen ? faChevronUp : faChevronDown} />
+              </Button>
+            </Col>
+          </Row>
         </Col>
       </Row>
       <Row>
-        <Collapse isOpen={open}>
+        <Collapse isOpen={awayOpen}>
           <div style={{ backgroundColor: "lightgray" }}>
-            {props.users.map((user) => (
+            {getWhoPickedGameResult(GameResult.AwayWin).map((user) => (
               <p key={props.game.id?.toString()! + "-" + user?.id?.toString()!}>
                 {user?.name}
               </p>
@@ -139,24 +143,35 @@ function CollapsibleTeamAndPicks(props: CollapsibleTeamAndPicksProps) {
           </div>
         </Collapse>
       </Row>
-    </Container>
-  ) : (
-    <Container>
-      <Row>
-        <Col className="col-4">
-          <Button onClick={() => setOpen(!open)}>{props.users.length}</Button>
-        </Col>
+      <Row className="align-items-center">
         <Col
           className={getClassName(GameResult.HomeWin)}
-          style={{ backgroundColor: getStyle(GameResult.HomeWin) }}
+          style={{ backgroundColor: getStyle(props.game, GameResult.HomeWin) }}
         >
           <TeamDisplay id={props.game.homeTeamId!} />
         </Col>
+        <Col className="col-3">
+          <h4>{averageScore(props.game, GameResult.HomeWin)} pts</h4>
+        </Col>
+        <Col className="col-3">
+          <Row>
+            <Col className="col-6">
+              <h4>
+                {getWhoPickedGameResult(GameResult.HomeWin).length}
+              </h4>
+            </Col>
+            <Col className="col-6">
+              <Button onClick={() => setHomeOpen(!homeOpen)}>
+                <FontAwesomeIcon icon={homeOpen ? faChevronUp : faChevronDown} />
+              </Button>
+            </Col>
+          </Row>
+        </Col>
       </Row>
       <Row>
-        <Collapse isOpen={open}>
+        <Collapse isOpen={homeOpen}>
           <div style={{ backgroundColor: "lightgray" }}>
-            {props.users.map((user) => (
+            {getWhoPickedGameResult(GameResult.HomeWin).map((user) => (
               <p key={props.game.id?.toString()! + "-" + user?.id?.toString()!}>
                 {user?.name}
               </p>
